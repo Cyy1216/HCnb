5beb1f01055c4e27ddebf93b32e81beb7035d3fa
huxinjian <1523332452@qq.com>
2020-04-06 00:37:54 +0800

    4_5

---
+++ t-sne_visual/julei.py
@@ -0,0 +1,100 @@
+#-*- coding:utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import skfuzzy as fuzz
+
+true_img_features_path = 'file/img_fake_6.npy'
+fake_img_features_path = 'file/img_true_6.npy'
+file_path = '../DM-GAN/code/file_6000.npz'
+
+way = 'kmeans' # kmeans or fzcmeans
+class_num = 50
+ 
+def cluster(img, cluster_num, way):
+    if way == 'kmeans':
+        return KMeans(n_clusters=cluster_num).fit(img)
+    if way == 'fzcmeans':
+        return fuzz.cmeans(img.T,c=cluster_num,m=2,error=0.0000005, maxiter=3000)[0]
+
+def get_pred(img, model, way):
+    if way == 'kmeans':
+        return model.predict(img)
+    if way == 'fzcmeans':
+        u, _, _, _, _, _ = fuzz.cluster.cmeans_predict(img.T, model, 2, error=0.0000005, maxiter=3000)[0]
+        return np.argmax(u, axis=0)
+
+def find_class_id(i, label_name, label_cluster, true_label_name):
+    num = np.bincount(label_cluster[i])
+    c_num = np.argmax(num)
+    while c_num in label_name:
+        max_num_a = np.max(np.bincount(label_cluster[label_name.index(c_num)]))
+        max_num_b = np.max(num)
+        if max_num_a >= max_num_b:
+            num[c_num] = 0
+            c_num = np.argmax(num)
+            if np.sum(num) == 0:
+                for j in true_label_name:
+                    if j not in label_name:
+                        label_name[i] = j
+                        break
+                break
+        else:
+            t_index = label_name.index(c_num)
+            label_name[t_index] = -1
+            label_name[i] = c_num
+            label_name = find_class_id(t_index, label_name, label_cluster, true_label_name)
+            break
+    if c_num not in label_name: label_name[i]=c_num
+    return label_name
+
+def get_cluster_name(pred, label):
+    true_label_name = np.unique(label)
+    label_cluster =  {}  #统计每个簇包含的类别
+
+    for i in range(len(pred)):
+        if pred[i] not in label_cluster:
+            label_cluster[pred[i]] = [label[i]]
+        else:
+            label_cluster[pred[i]].append(label[i])
+
+    label_name = []
+    for i in range(len(label_cluster)):
+        label_name.append(-1)
+        label_name = find_class_id(i, label_name, label_cluster, true_label_name)
+    return label_name
+
+def main():
+    fake_img = np.load(true_img_features_path)
+    true_img = np.load(fake_img_features_path)
+    file_img = np.load(file_path)
+    label = file_img['class_id']
+    
+    #生成按真实标签划分颜色的图片
+    plt.scatter(true_img[:, 0], true_img[:, 1], c=label[:len(true_img)], s=3)
+    plt.savefig('by_class.png')
+    plt.close()
+
+    #聚类
+    model = cluster(true_img, class_num, way)
+    pred_true = get_pred(true_img, model, way)
+    #为每个簇分配标签
+    label_name = get_cluster_name(pred_true, label)
+    #获取生成图片的预测
+    pred_false = get_pred(fake_img, model, way)
+
+    color = []
+    for i in range(len(pred_false)):
+        if label_name[pred_false[i]] != label[i]:
+            color.append('#FF0000')
+        else:
+            color.append('#0000FF')
+
+    plt.scatter(true_img[:, 0], true_img[:, 1], c='#000000', s=3)
+    plt.scatter(fake_img[:, 0], fake_img[:, 1], c=color, s=3)
+    plt.savefig('result.png')
+
+
+if __name__ == "__main__":
+    main()
+
--- t-sne_visual/visual.py
+++ t-sne_visual/visual.py
@@ -1,15 +1,20 @@
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from matplotlib import pyplot as plt 
+import numpy as np
 
 import os
 import sys
 import scipy.misc
-sys.path.append('inception_model')
-from extract_img import extract_img
+sys.path.append('inception')
+sys.path.append('/home/huxinjian/workspace/inception_cub')
+from extract_img import extract_img, preprocess
 
 
-img_path = "E:\\dataset\\bird\\CUB_200_2011\\images"
+path = "../DM-GAN/code/file_6000.npz"
+PCA_dim = 100
+TSNE_dim = 2
+T_perplexity = 120
 
 def load_data(fullpath):
     print(fullpath)
@@ -21,21 +26,76 @@ def load_data(fullpath):
                 if os.path.isfile(filename):
                     img = scipy.misc.imread(filename)
                     images.append(img)
-        if len(images)>5000:
+        if len(images)>500:
             break
     print('images', len(images), images[0].shape)
     return images
 
+def load_img(fake_img, true_img):
+    fake_images = []
+    true_images = []
+    for path in fake_img:
+        path = '../DM-GAN/' + path[path.find('model'):]
+        img = scipy.misc.imread(path)
+        fake_images.append(img)
+    print(len(fake_images))
+    for path in true_img:
+        path = '../DM-GAN/' + path[path.find('model'):]
+        img = scipy.misc.imread(path)
+        true_images.append(img)
+    print(len(true_images))
+    return [fake_images, true_images] 
+
 def main():
-    pca=PCA(n_components=100)
-    images = load_data(img_path)
-    img_pre = extract_img(images)
-    img_pca = pca.fit_transform(img_pre)
-    img_X = TSNE(n_components=2).fit_transform(img_pca)
-    # print(img_X.shape)
-    ax = plt.gca()
-    ax.scatter(img_X[:,0],img_X[:,1])
-    plt.savefig('./test.jpg')
+    pca_fake=PCA(n_components=PCA_dim)
+    pca_true=PCA(n_components=PCA_dim)
+
+    img_path  = np.load(path)
+    class_ids, fake_path, true_path = img_path['class_id'], img_path['fake_path'], img_path['true_path']
+
+    images = load_img(fake_path, true_path)
+
+    true_img = []
+    fake_img = []
+
+    i=0
+    while True:
+        if len(images[0]) - i < 1000:
+            img = images[0][i:]
+        else:
+            img = images[0][i:i+1000]
+        i += 1000
+        img = extract_img(img)
+        true_img.append(img)
+        if i >= len(images[0]):break
+    
+    i = 0
+    while True:
+        if len(images[1]) - i < 1000:
+            img = images[1][i:]
+        else:
+            img = images[1][i:i+1000]
+        i += 1000
+        img = extract_img(img)
+        fake_img.append(img)
+        if i>= len(images[1]):break
+    
+    true_img = np.vstack(true_img)
+    fake_img = np.vstack(fake_img)
+    print(true_img.shape, fake_img.shape)
+
+    img_pca = pca_fake.fit_transform(true_img)
+    img_X = TSNE(n_components=TSNE_dim, perplexity=TSNE_dim).fit_transform(img_pca)
+ 
+    img_pca = pca_true.fit_transform(fake_img)
+    img_Y = TSNE(n_components=TSNE_dim, perplexity=TSNE_dim).fit_transform(img_pca)
+
+    np.save('img_fake.npy', img_X)
+    np.save('img_true.npy', img_Y)
+
+    plt.scatter(img_X[:,0],img_X[:,1],s=8,color=(0.,0.5,0.))
+    plt.scatter(img_Y[:,0],img_Y[:,1],s=8,color=(0.8,0,0.))
+    plt.show()
 
 if __name__ == '__main__':
     main()
